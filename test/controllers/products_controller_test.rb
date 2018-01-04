b84954dca78770b7608ca63aaee8ca1e420a7333
RickyCode <rdelrioguzman@gmail.com>
2018-01-04 19:45:36 -0300

    Cart Creation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,21 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
          price: 19.95
      }
   end
+
+  test "can't delete product in cart" do
+     assert_difference('Product.count', 0) do
+       delete product_url(products(:two))
+       end
+     assert_redirected_to products_url
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+    assert_redirected_to products_url
+  end
+  
   test "should get index" do
     get products_url
     assert_response :success
