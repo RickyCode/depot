67105d662b4b3c5843655503dfdbe1e8bc2239da
RickyCode <rdelrioguzman@gmail.com>
2018-01-06 23:54:15 -0300

    adding a button

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,17 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    puts 'PRODUCT:' + product.to_s
+    current_item = line_items.find_by(product_id: product.id)
+    # current_item = line_items.find(product_id: product.id)[0]
+
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+    current_item
+  end
+
 end
