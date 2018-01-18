b8f63c165ffde2fdbaab98f504948b1471a32089
RickyCode <rdelrioguzman@gmail.com>
2018-01-18 13:10:24 -0300

    Iteration F2: Creating an Ajax-Based Cart (no funciona por alguna razon)

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -67,6 +67,12 @@ class CartsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
+      if @cart.id == session[:cart_id]
+        @cart
+      else
+        invalid_cart
+      end
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,9 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         # format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.html { redirect_to @line_item.cart}
+        # format.html { redirect_to @line_item.cart}
+        format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
