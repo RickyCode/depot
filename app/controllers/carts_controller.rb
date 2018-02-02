a9042b878e8f74a31483a8184e5c2b4c457e2f71
RickyCode <rdelrioguzman@gmail.com>
2018-02-01 22:23:57 -0300

    Iteration I3: Limiting Acces
    Se modifica el controlador de la aplicación para que antes de cualquier acción verifique que un administrador haya iniciado sesión. En cada controlador que no requiere de permisos de administrador se añade una excepción a dicha verificación. Se modifica el test helper para solcionar los erroes que se preoducen en los test integrados al añadir el inicio de sesión

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
+  skip_before_action :authorize
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
